fa889b94ebe24ca76aa5bbbd41b796244820ddb5
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-12 12:46:24 +0200

    tp 04

--- src/main/java/fr/algorithmie/InversionContenu.java
+++ src/main/java/fr/algorithmie/InversionContenu.java
@@ -1,25 +1,25 @@
 package fr.algorithmie;
 
-public class AffichageInverse {
+public class InversionContenu {
 
     public static void main(String[] args) {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        //Affichage du tableau array
+        System.out.println("Affichage du tableau array :");
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[array.length - 1 - i]);
-            }
-        //Affichage inverse
-        for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
-        // Création d'une copie du tableau array
+        //Creation d'un tableau arrayCopy en ordre inverse
         int[] arrayCopy = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            arrayCopy[i] = array[i];
+            arrayCopy[i] = array[array.length - 1 - i];
         }
-        // Affichage du tableau copie
-        System.out.println("\nAffichage du tableau copie :");
+        //Affichage du tableau arrayCopy
+        System.out.println("\nAffichage du tableau arrayCopy :");
         for (int i = 0; i < arrayCopy.length; i++) {
             System.out.println(arrayCopy[i]);
         }
+
     }
 }
