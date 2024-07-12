788a7b189f948aad2f0aed335797a5b4016d7e36
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-12 11:36:53 +0200

    2eme tp

---
+++ src/main/java/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,26 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte myByte = 10;
+        short myShort = 20;
+        int myInt = 30;
+        long myLong = 40L;
+        float myFloat = 50.5f;
+        double myDouble = 60.6;
+
+        char myChar = 'A';
+        boolean myBoolean = true;
+        String myString = "Bonjour";
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("String: " + myString);
+    }
+}
