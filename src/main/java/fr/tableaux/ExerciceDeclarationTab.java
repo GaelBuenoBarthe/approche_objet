18b29780d2273e87c8fe41443face8c3ab32698e
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-12 11:54:52 +0200

    tp 3

---
+++ src/main/java/fr/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,21 @@
+package fr.tableaux;
+
+public class ExerciceDeclarationTab {
+    public static void main(String[] args) {
+        int[] tab = new int[8];
+        tab[0] = 8;
+        tab[1] = -7;
+        tab[2] = 12;
+        tab[3] = 1;
+        tab[4] = -2;
+        tab[5] = 14;
+        tab[6] = 17;
+        tab[7] = 19;
+
+        // Affichage de l'élément à l'index 0
+        System.out.println("Element de index 0: " + tab[0]);
+
+        //Affichage de la longuer du tableau
+        System.out.println("Longueur du tableau: " + tab.length);
+    }
+}
