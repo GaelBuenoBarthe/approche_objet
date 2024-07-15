efa6a455416183bdcf70a59f881bf1661a8b1bf6
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-15 14:29:20 +0200

    .

--- src/main/java/entites2/Personne.java
+++ src/main/java/entites2/Personne.java
@@ -16,29 +16,23 @@ public class Personne {
     }
 
     public String getNom() {
-
         return nom;
     }
     public void setNom(String nom) {
-
         this.nom = nom;
     }
 
     public String getPrenom() {
-
         return prenom;
     }
     public void setPrenom(String prenom) {
-
         this.prenom = prenom;
     }
 
     public AdressePostale getAdressePostale() {
-
         return adressePostale;
     }
     public void setAdressePostale(AdressePostale adressePostale) {
-
         this.adressePostale = adressePostale;
     }
 }
