42fe2bb2d76ca72b80ca17fa36ce15e258b1b5ca
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-15 11:24:51 +0200

    fin tp 8

--- src/main/java/utils/TestMethodeStatic.java
+++ src/main/java/utils/TestMethodeStatic.java
@@ -1,4 +1,20 @@
 package utils;
 
 public class TestMethodeStatic {
+
+    public static void main(String[] args) {
+        String chaine = "12";
+
+        int nombre = Integer.parseInt(chaine);
+
+        int a = 15;
+        int b = 23;
+
+        // Utilisez une méthode de la classe Integer qui calcule le nombre maximum entre a et b.
+        int max = Integer.max(a, b);
+
+        // Affichez le résultat de l’appel de cette méthode.
+        System.out.println("Le nombre converti est : " + nombre);
+        System.out.println("Le maximum entre " + a + " et " + b + " est : " + max);
+    }
 }
